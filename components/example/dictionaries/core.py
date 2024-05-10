1a3f3dd924bcef3b85d3a0bc7df89fd177fac148
David Vujic <david@vujic.se>
2024-05-10 17:34:26 +0200

    add the example bricks and projects from the Polylith example repo

---
+++ components/example/dictionaries/core.py
@@ -0,0 +1,17 @@
+"""
+Dictionary helper functions
+
+The pick and omit functions are similar to select-keys and dissoc in Clojure,
+and I have borrowed the naming from the JavaScript equivalents in the Lodash.js library.
+
+"""
+
+import awsglue
+
+
+def pick(data: dict, keys: set) -> dict:
+    return {k: v for k, v in data.items() if k in keys}
+
+
+def omit(data: dict, keys: set) -> dict:
+    return {k: v for k, v in data.items() if k not in keys}
